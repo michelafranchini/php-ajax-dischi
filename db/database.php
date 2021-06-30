14479640ae4748351215cd5f6e51680c90810b9e
michelafranchini <michela.franchini.91@gmail.com>
2021-06-30 16:54:28 +0200

    Inizio crazione database

---
+++ db/database.php
@@ -0,0 +1,33 @@
+<?php 
+    $discs = [
+        [
+            "title" => "The Resistance", 
+            "image" => "https://images-na.ssl-images-amazon.com/images/I/91ZwmJUvFDL._AC_SL1500_.jpg", 
+            "author" => "Muse", 
+            "year" => "2009", 
+            "genre" => "Rock"
+        ], 
+        [
+            "title" => "Violator", 
+            "image" => "https://images.wired.it/wp-content/uploads/2020/03/13165814/DM___Violator_LP_by_IDAlizes-1.png", 
+            "author" => "Depeche Mode", 
+            "year" => "1990", 
+            "genre" => "Rock"
+        ], 
+        [
+            "title" => "Maximum Overload", 
+            "image" => "https://cadiesart.com/images_gallery/cdpackaging/DragonForce_MaximumOverloadDigiPack/DFMODP_Art02.jpg", 
+            "author" => "DragonForce", 
+            "year" => "2014", 
+            "genre" => "Metal"
+        ],
+        [
+            "title" => "Maximum Overload", 
+            "image" => "https://cadiesart.com/images_gallery/cdpackaging/DragonForce_MaximumOverloadDigiPack/DFMODP_Art02.jpg", 
+            "author" => "DragonForce", 
+            "year" => "2014", 
+            "genre" => "Metal"
+        ],
+
+    ]
+?>
